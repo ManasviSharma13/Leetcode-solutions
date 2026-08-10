c3f5709e83b07e74cfd0a20a7dded8575e805dc7
ManasviSharma13 <smanasvi1309@gmail.com>
2026-08-10 17:58:13 +0530

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/88-MergeSortedArray.cpp
@@ -0,0 +1,24 @@
+// Last updated: 10/08/2026, 17:58:13
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+      int a=m-1;
+      int b= n - 1;
+      int k=m+n-1;
+      while(a>=0&&b>=0){
+        if(nums1[a]>nums2[b]){
+            nums1[k]=nums1[a];
+            a--;        
+            }else{
+                nums1[k]=nums2[b];
+                b--;
+            }
+            k--;
+      }
+      while(b>=0){
+        nums1[k]=nums2[b];
+        k--;
+        b--;
+      }
+    }
+};
